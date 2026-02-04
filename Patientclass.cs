9a3be849e7e3e53b0553618a2c63b3d7614ee684
Kenda K <137882184+kenkel206@users.noreply.github.com>
2026-02-04 14:45:47 +0000

    patient class
    patient class with variables

---
+++ Patientclass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Patient
+{
+	public Patient()
+	{
+		string name;
+		bool isongoing;
+		List <string> allergies = new List<string>();
+        List <string> medicalConditions = new List<string>();
+	}
+}
