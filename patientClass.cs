6c4b1ebd290ab4f1bb8988cc8782b0225638762e
Kenda K <137882184+kenkel206@users.noreply.github.com>
2026-02-04 15:44:09 +0000

    Refactor Patient class with additional properties and methods
    Refactor Patient class to include first and last name, ongoing status, and lists for allergies and medical conditions. Added constructors and getter/setter methods.

--- patientClass.cs
+++ patientClass.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 
-public class Patient
-{
-	public Patient()
-	{
-		string name;
-		bool isongoing;
+public class Patient{
+		string firstName;
+		string lastName;
+		bool isOngoing;
 		List <string> allergies = new List<string>();
         List <string> medicalConditions = new List<string>();
+	
+	//no args constructor for patient class
+	public Patient(){
 	}
+
+    //args constructor for patient class 
+    public Patient(string fname, string lname, bool status, List<string> allergies, List<string> medicalConditions){
+		this.firstName = fname;
+		this.lastName = lname;
+		this.isOngoing = status;
+		this.allergies = allergies;
+		this.medicalConditions = medicalConditions;
+
+    }
+
+    //setter methods for patients 
+    private void setFirstName(string fname){
+		this.firstName = fname;
+    }
+	private void setLastName(string lname) {
+		this.lastName = lname;
+    }
+
+	private void setOngoingStatus(bool status) {
+		this.isOngoing = status;
+    }
+	private void addAllergy(string allergy){
+		this.allergies.Add(allergy);
+    }
+	private void addMedicalCondition(string condition){
+		this.medicalConditions.Add(condition);
+    }
+    //getter methods for patients
+	private string getFirstName(){
+		return this.firstName;
+    }
+	private string getLastName(){
+		return this.lastName;
+    }
+	private bool getOngoingStatus(){
+		return this.isOngoing;
+    }
+	private List<string> getAllergies(){
+		return this.allergies;
+    }
+	private List<string> getMedicalConditions() {
+		return this.medicalConditions;
+    }
 }
+
