708af23b5a23b596613734eaa8db73f3b9abdc27
myrtlemill11 <myrtle.11miller@gmail.com>
2026-02-04 14:47:41 +0000

    Add files via upload

---
+++ Dentist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classDemo1forProject
+{
+    internal class Dentist
+    {
+        private String name;
+        private SqlDateTime availibility;
+        private String speciality;
+        private List<Patient>  personalPatients;
+    }
+}
