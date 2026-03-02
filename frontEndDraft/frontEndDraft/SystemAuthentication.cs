bfdd8d188c5da534cd99094eb396db78b368235a
myrtlemill11 <myrtle.11miller@gmail.com>
2026-03-02 15:55:43 +0000

    Add email authentication code feature
    Implemented email authentication code sending functionality.

--- frontEndDraft/frontEndDraft/SystemAuthentication.cs
+++ frontEndDraft/frontEndDraft/SystemAuthentication.cs
@@ -19,7 +19,15 @@ namespace frontEndDraft
 
         private void authenticate_Click(object sender, EventArgs e)
         {
-
+            // send user email an authentication code that they can enter into the system
+            User user = new User();
+            string email = user.getEmail();
+            string from = user.getEmail();
+            MailMessage message = new MailMessage(from, email);
+            message.Subject = "Authentication code";
+            message.Body = "Authentication code is 1234";
+            SmtpClient client = new SmtpClient();
         }
     }
 }
+
