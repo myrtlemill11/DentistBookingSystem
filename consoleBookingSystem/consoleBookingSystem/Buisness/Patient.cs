c21abd6eddbafcdc3b4bb43d0c6d96167bd2404a
myrtlemill11 <myrtle.11miller@gmail.com>
2026-03-08 17:12:48 +0000

    New System Draft based on new plan
    When editing, download and test any new files with the whole system

---
+++ consoleBookingSystem/consoleBookingSystem/Buisness/Patient.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleBookingSystem.Buisness
+{
+    internal class Patient : User
+    {
+        // data fields
+        private List<string> allergies = new List<string>();
+        private List<string> medicalConditions = new List<string>();
+        private Dentist dentist;
+        private List<Booking> appointments = new List<Booking>();
+        private DateTime DOB;
+        private string typeOfPatient;
+
+        // getter methods
+        public Dentist getDentist()
+        {
+            return dentist; 
+        }
+
+        public List<string> getMedicalConditions()
+        {
+            return medicalConditions;
+        }
+        public List<Booking> getAppointments()
+        {
+            return appointments;
+        }
+
+        public Booking getAppointment(Booking b)
+        {
+            return b;
+        }
+
+        public List<string> getAllergies()
+        {
+            return allergies;
+        }
+
+        public string getTypeOfPatient()
+        {
+            return typeOfPatient;
+        }
+
+        // methods to change/add bookings
+        public void requestCancellation()
+        {
+
+        }
+
+        public void bookAppointment()
+        {
+
+        }
+
+
+    }
+}
---
+++ consoleBookingSystem/consoleBookingSystem/Presentation/ApplicationRunner.cs
@@ -0,0 +1,19 @@
+﻿using consoleBookingSystem.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace consoleBookingSystem
+{
+    public class ApplicationRunner
+    {
+        static void Main(string[] args)
+        {
+            View v = new View();
+            v.startUI();
+        }
+    }
+}
