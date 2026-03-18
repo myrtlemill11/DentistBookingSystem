6bf42196c7f55de9210faee614bb1e648c5815d0
myrtlemill11 <myrtle.11miller@gmail.com>
2026-03-18 15:28:23 +0000

    Implement SQL data access methods for bookings

--- consoleBookingSystem2/consoleBookingSystem2/Data/SQLDAOImplemetation.cs
+++ consoleBookingSystem2/consoleBookingSystem2/Data/SQLDAOImplemetation.cs
@@ -1,12 +1,70 @@
-﻿using System;
+using consoleBookingSystem.Buisness;
+using Microsoft.Data.SqlClient;
+using System.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace consoleBookingSystem.Data
 {
     public class SQLDAOImplementation : DAO
     {
+        private string connString = ConfigurationManager
+.ConnectionStrings["AppointmentsDB"].ConnectionString;
+
+    public List<Booking> viewAppointments()
+    {
+
+        using (var conn = new SqlConnection(connString))
+        {
+
+            return conn.Query<Booking>("SELECT * FROM Bookings ORDER BY LastName")
+                       .ToList();
+        }
+    }
+    public int InsertAppointment(Booking Booking)
+    {
+        using (var conn = new SqlConnection(connString))
+        {
+            string sql = @"INSERT INTO Bookings
+                   (date, dentist, patient, reasonForAppt, priorityLevel)
+                   VALUES (@date, @dentist, @patient, @reasonForAppt, @priorityLevel);
+                   SELECT CAST(SCOPE_IDENTITY() AS INT);";
+
+            int newId = conn.ExecuteScalar<int>(sql, Booking);
+            return newId;
+        }
+    }
+    public int UpdateContact(Booking booking)
+    {
+        using (var conn = new SqlConnection(connString))
+        {
+            string sql = @"UPDATE Bookings SET
+                    date = @date,
+                    dentist = @dentist,
+                    patient = @patient,
+                    reasonForAppt = @reasonForAppt,
+                    priorityLevel = @priorityLevel
+                    WHERE BookingId = @BookingId";
+
+            return conn.Execute(sql, booking);
+        }
+    }
+
+    public int DeleteAppointment(int BookingId)
+    {
+        using (var conn = new SqlConnection(connString))
+        {
+            string sql = "DELETE FROM Bookings WHERE BookingId = @Id";
+            return conn.Execute(sql, new { Id = BookingId });
+        }
+    }
+
+
+
+}
     }
 }
