6ecb9dc615e4b0efdee8818b057cd08a96812ab3
myrtlemill11 <myrtle.11miller@gmail.com>
2026-03-24 22:19:26 +0000

    Add deleteAppointment function, delete "assignPatient" and deletePatient.cs

--- consoleBookingSystem2/consoleBookingSystem2/Data/SQLDAOImplemetation.cs
+++ consoleBookingSystem2/consoleBookingSystem2/Data/SQLDAOImplemetation.cs
@@ -113,71 +113,38 @@ namespace consoleBookingSystem.Data
             }
         }
 
-        public int DeleteAppointment(int BookingId)
+        public int insertUser(User user)
         {
-            using (var conn = new Microsoft.Data.SqlClient.SqlConnection(connString))
+        using (var conn = new Microsoft.Data.SqlClient.SqlConnection(connString))
             {
-                string sql = "DELETE FROM Bookings WHERE BookingId = @Id";
-                return conn.Execute(sql, new { Id = BookingId });
-            }
-        }
-        public int InsertPatient(Patient patient)
-        {
-            using (var conn = new Microsoft.Data.SqlClient.SqlConnection(connString))
-            {
-                string sql = @"INSERT INTO Patients
-                   (allergies, medicalConditions, dentist,appointments, DOB, typeOfPatient, id)
-                   VALUES (@allergies, @medicalConditions, @dentist, @appointments, @DOB, @typeOfPatient, @id);
-                   SELECT CAST(SCOPE_IDENTITY() AS INT);";
-                int newId = conn.ExecuteScalar<int>(sql, patient);
+                string sql = @"INSERT INTO Users
+               (firstName, lastName, id, password, email, phoneNumber)
+               VALUES (@firstName, @lastName, @id, @password, @email, @phoneNumber);
+               SELECT CAST(SCOPE_IDENTITY() AS INT);";
+                int newId = conn.ExecuteScalar<int>(sql, new
+                {
+                    firstName = user.getFirstName(),
+                    lastName = user.getLastName(),
+                    id = user.getId(),
+                    password = user.getPassword(),
+                    email = user.getEmail(),
+                    phoneNumber = user.getPhoneNumber(),
+                });
                 return newId;
 
 
             }
-        }
-        public int DeletePatient(int id)
-        {
-            using (var conn = new Microsoft.Data.SqlClient.SqlConnection(connString))
-            {
-                string sql = "DELETE FROM Patients WHERE id = @Id";
-                return conn.Execute(sql, new { Id = id });
-            }
-        }
-        public int assignPatient(Patient patient, Dentist dentist)
-        {
-            using (var conn = new Microsoft.Data.SqlClient.SqlConnection(connString))
-            {
-                string sql = @"UPDATE Patients SET
-                    dentist = @dentist
-                    WHERE id = @id";
-                return conn.Execute(sql, new { dentist = dentist, id = patient.getId() });
-            }
-            }
-        }
 
-        public int insertUser(User user)
+        }
+        public int DeleteAppointment(DateTime date) 
         {
         using (var conn = new Microsoft.Data.SqlClient.SqlConnection(connString))
-        {
-            string sql = @"INSERT INTO Users
-               (firstName, lastName, id, password, email, phoneNumber)
-               VALUES (@firstName, @lastName, @id, @password, @email, @phoneNumber);
-               SELECT CAST(SCOPE_IDENTITY() AS INT);";
-            int newId = conn.ExecuteScalar<int>(sql, new
             {
-                firstName = user.getFirstName(),
-                lastName = user.getLastName(),
-                id = user.getId(),
-                password = user.getPassword(),
-                email = user.getEmail(),
-                phoneNumber = user.getPhoneNumber(),
-            });
-            return newId;
-
-
+                 string sql = "DELETE FROM Appointments WHERE [date] = @date";
+                 return conn.Execute(sql, new { date });
+            }
+    
         }
-
-    }
          
         public List<string> viewUsers()
         {
