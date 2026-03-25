53877c23b01dc323d5835050d6ff137f137e854d
myrtlemill11 <myrtle.11miller@gmail.com>
2026-03-25 13:27:52 +0000

    Add dashboard function to admin

--- consoleBookingSystem2/consoleBookingSystem2/Business/Admin.cs
+++ consoleBookingSystem2/consoleBookingSystem2/Business/Admin.cs
@@ -33,6 +33,74 @@ namespace consoleBookingSystem.Buisness
             }
         }
 
+        // get dashboard for using functions
+public void getDashboard() {
+    // set int value for input
+    int input = 0;
+
+    // present user options
+    Console.WriteLine("Admin Dashboard");
+    Console.WriteLine("Select an option by typing the numerical value:");
+    Console.WriteLine("1. View Schedule");
+    Console.WriteLine("2. View Dentist Schedule");
+    Console.WriteLine("3. Reschedule Appointment");
+    Console.WriteLine("4. Cancel Appointment");
+    Console.WriteLine("5. Exit");
+
+    // get user response and call appropriate method
+    try
+    {
+      input = int.Parse(Console.ReadLine());
+    } catch (Exception e) 
+    { 
+        Console.WriteLine("Please try again"); 
+        getDashboard();
+    }
+
+    switch (input)
+    {
+        case 1:
+            viewSchedule();
+            break;
+
+        case 2:
+            viewDentistSchedule();
+            break;
+
+        case 3:
+            Console.WriteLine("Enter booking date to reschedule:");
+            try
+            {
+                DateTime d = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Enter new date:");
+                DateTime newDate = DateTime.Parse(Console.ReadLine());
+
+                reschedule(dao.viewAppointment(d), newDate);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Please try again");
+                getDashboard();
+            }
+            break;
+
+        case 4:
+            Console.WriteLine("Enter an appointment to cancel:");
+            cancel(DateTime.Parse(Console.ReadLine()));
+            break;
+        case 5:
+            Console.WriteLine("Exiting system...");
+            break;
+
+        default:
+            Console.WriteLine("Invalid option. Try again.");
+            break;
+    }
+
+
+
+}
         public void viewDentistSchedule() {
             Dentist dentist = new Dentist();
 
