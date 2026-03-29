b712865334afe1a386dd219e7f1c7aabc7343c1a
myrtlemill11 <myrtle.11miller@gmail.com>
2026-03-29 23:05:24 +0100

    Implement viewPatientAppointments method
    Added method to view patient appointments.

--- consoleBookingSystem2/consoleBookingSystem2/Data/SQLDAOImplemetation.cs
+++ consoleBookingSystem2/consoleBookingSystem2/Data/SQLDAOImplemetation.cs
@@ -17,16 +17,30 @@ namespace consoleBookingSystem.Data
         private string connString = ConfigurationManager
             .ConnectionStrings["AppointmentsDB"].ConnectionString;
 
-public List<string> viewDentistAppointments(Dentist d) // needs to be fixed
-{
-    // get dentist json to find appointment
-    var dentistJson = Newtonsoft.Json.JsonConvert.SerializeObject(d);
-    using (var conn = new Microsoft.Data.SqlClient.SqlConnection(connString))
+    public List<string> viewDentistAppointments(Dentist d) // needs to be fixed
     {
-        string sql = "SELECT * FROM Appointments WHERE JSON_VALUE([dentist], '$.id') = @id";
-        return conn.Query<string>(sql, new { id = d.getId() }).ToList();
+        // get dentist json to find appointment
+        var dentistJson = Newtonsoft.Json.JsonConvert.SerializeObject(d);
+        using (var conn = new Microsoft.Data.SqlClient.SqlConnection(connString))
+        {
+            string sql = "SELECT * FROM Appointments WHERE JSON_VALUE([dentist], '$.id') = @id";
+            return conn.Query<string>(sql, new { id = d.getId() }).ToList();
+        }
     }
-}
+
+        public List<string> viewPatientAppointments(Patient p) // works
+        {
+            // get patient json to find appointment
+            var patientJson = Newtonsoft.Json.JsonConvert.SerializeObject(p);
+
+            using (var conn = new Microsoft.Data.SqlClient.SqlConnection(connString))
+            {
+
+                string sql = "SELECT * FROM Appointments WHERE JSON_VALUE([patient], '$.id') = @id";
+               
+                return conn.Query<string>(sql, new { id = p.getId() }).ToList();
+            }
+        }
 
         public List<Patient> GetAllPatients()
         {
