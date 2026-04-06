6d1e7a05e4c928b23a126559da66279f6a1bd832
myrtlemill11 <myrtle.11miller@gmail.com>
2026-04-06 12:53:18 +0100

    Fix namespace typo in BookingManager.cs

--- consoleBookingSystem2/consoleBookingSystem2/Business/BookingManager.cs
+++ consoleBookingSystem2/consoleBookingSystem2/Business/BookingManager.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using consoleBookingSystem.Data;
 
-namespace consoleBookingSystem2.Buisness
+namespace consoleBookingSystem.Buisness
 {
     public class BookingManager
     {
